4862bc9c3014a9ff24ff47f04766bda532d75b1d
agent-52 <abhaybhadauriya52@gmail.com>
2024-02-11 00:34:36 +0530

    complete but can be better

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ const pageChoice = document.querySelector("#pages");
 const errorMessage = document.querySelector(".errorMessage");
 const bookstore = document.querySelector(".bookstore")
 const pageInput = document.querySelector(".pageinput")
+const DeleteButton = document.querySelector(".deleteButton")
+const CompleteButton = document.querySelector(".completeButton")
 
 const myLibrary = [];
 
@@ -24,16 +26,15 @@ let title;
 let author;
 let genre;
 let page;
+let bookName;
 
 function addBookToLibrary(){
+
   title = titleChoice.value;
-  let bookName = title
   author = authorChoice.value;
   genre = genreChoice.value;
   page = pageChoice.value;
-
   bookName = new Book(title, author, genre, page)
-
   myLibrary.push(bookName);
   console.log(myLibrary)
   console.log(bookName.author)
@@ -42,18 +43,45 @@ function addBookToLibrary(){
 }
 
 function showBook(){
+
   const div =document.createElement('div');
   div.classList.add("bookcard");
-  div.innerHTML = `<h1>${title}</h1>
-  <div><span>author: </span>${author}</div>
-        <div><span>Genre:</span> ${genre}</div>
-        <div><span>Current page:</span><input type="text" value="${page}" class="pageinput"/></div>
-        <button>mark read</button>`;
+  const h1 = document.createElement('h1')
+  h1.textContent = title;
+  const div1 = document.createElement('div')
+  div1.innerHTML = `<span>author:</span> ${author}`
+  const div2 = document.createElement('div')
+  div2.innerHTML = `<span>Genre:</span> ${genre}`
+  const div3 = document.createElement('div')
+  div3.innerHTML = `<span>Current page:</span> ${page}`
+  const completeButton = document.createElement('button')
+  completeButton.classList.add("completeButton")
+  completeButton.textContent = "Mark Complete"
+
+  const deleteButton = document.createElement('button')
+  deleteButton.classList.add("deleteButton")
+  deleteButton.textContent = "Delete"
+
+  div.appendChild(h1)
+  div.appendChild(div1)
+  div.appendChild(div2)
+  div.appendChild(div3)
+  div.appendChild(completeButton)
+  div.appendChild(deleteButton)
+  
 
   bookstore.appendChild(div)
 
-};
+  deleteButton.addEventListener("click", () =>{bookstore.removeChild(div)});
 
+  completeButton.addEventListener("click", () =>{
+    const completed = document.createElement('div')
+    completed.innerHTML = `<span>Status: </span>Completed`
+    div.insertBefore(completed, completeButton)
+    div.removeChild(completeButton)
+  })
+
+};
 
 addButton.addEventListener("click", () =>{
   dialog.showModal();
@@ -63,11 +91,25 @@ confirmAdd.addEventListener("click", (event) => {
   
   event.preventDefault();
   addBookToLibrary();
-  showBook();
+  if(title != "" && author != "" && genre != "" && page != ""){
+    showBook();
+    errorMessage.textContent = ""
+    dialog.close();
+  }else{
+    errorMessage.textContent = "You need to fill all the above details !!"
+  }
+  
 
-  dialog.close();
+  
 
 });
+
+
+
+
+
+//function deleteBook()
+
 
 
 //if(genreChoice.value != "" && authorChoice.value != "" &&titleChoice.value != "" && pageChoice.value != ""){
